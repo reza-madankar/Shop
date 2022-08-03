870c9796f2d5c5ac1664dea710dd659577f510b8
Hooman <patriot_sh@yahoo.com>
2022-08-03 13:30:48 +0430

    header and navbar ok

--- src/Pages/Layout/header.jsx
+++ src/Pages/Layout/header.jsx
@@ -2,17 +2,61 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "../../Content/Style/custom.scss";
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
 
 function NavbarDarkExample() {
   return (
-  <div >
+  <div className='header' >
+ 
+ 
+    <Navbar>
+      <Container>
+        <Nav>
+       
+           <NavDropdown 
+              id="nav-dropdown-dark-example"
+              title="English"
+              menuVariant="dark">
+  <NavDropdown.Item href="#action/3.1">eNGLISH:</NavDropdown.Item>
+  <NavDropdown.Item href="#action/3.1">Features:</NavDropdown.Item>
+  <NavDropdown.Item href="#action/3.1">Features:</NavDropdown.Item>
+  <NavDropdown.Item href="#action/3.1">Features:</NavDropdown.Item>
+           </NavDropdown>
+           <p></p> 
+           <NavDropdown 
+              id="nav-dropdown-dark-example"
+              title="USD"
+              menuVariant="dark">
+  <NavDropdown.Item href="#action/3.1">eNGLISH:</NavDropdown.Item>
+  <NavDropdown.Item href="#action/3.1">Features:</NavDropdown.Item>
+  <NavDropdown.Item href="#action/3.1">Features:</NavDropdown.Item>
+  <NavDropdown.Item href="#action/3.1">Features:</NavDropdown.Item>
+           </NavDropdown>
+           <p> </p>
+           <NavDropdown 
+              id="nav-dropdown-dark-example"
+              title="Call us 3965410"
+              menuVariant="dark">
+                 </NavDropdown>
+                 <p1>Free delivery on order over <span> $200</span> </p1>
+        </Nav>
+      </Container>
+    </Navbar>
+   <h2></h2>
+   
+ 
+   
+ 
 
+  
     <Navbar  bg="white" expand="lg" >
-      <Container Class="container text-center">
+      <Container className="justify-content-md-center">
        <Nav >
+                  
+
             <NavDropdown 
               id="nav-dropdown-dark-example"
               title="Features"
@@ -88,10 +132,23 @@ function NavbarDarkExample() {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>  
+          
           </Nav>
+          
       </Container>
+        
+      <span> 
+        
+<div className='icon'>
+<i class="glyphicon glyphicon-cloud">/</i>
+<i class="glyphicon glyphicon-remove">/</i>
+<i class="glyphicon glyphicon-user">/</i>
+<i class="glyphicon glyphicon-envelope">/</i>
+<i class="glyphicon glyphicon-thumbs-up">/</i>
+    </div></span>
     </Navbar>
   
+
 
       </div>
   );

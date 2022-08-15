d15a3d62281b54575304b5d4649c0b6cf5d96953
Hooman <patriot_sh@yahoo.com>
2022-08-15 14:23:48 +0430

    complete menu

---
+++ src/Pages/Layout/component/topHeader.jsx
@@ -0,0 +1,34 @@
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+
+import "../../../Content/Style/header.scss";
+
+const TopHeader = () => {
+  return (
+    <div id="topHeader">
+      <div className="drpMenu">
+        <NavDropdown className="navItem" title="English">
+          <NavDropdown.Item href="#action/3.1">Arabic</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.1">Bangla</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.1">Hindi</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.1">Spanish</NavDropdown.Item>
+        </NavDropdown>
+
+        <NavDropdown className="navItem" title="USD">
+          <NavDropdown.Item href="#action/3.1">Taka (BDT)</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.1">Riyal (SAR)</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.1">Rupee (INR)</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.1">Dirham (AED)</NavDropdown.Item>
+        </NavDropdown>
+
+        <a href="#" className="navItem">Call Us 3965410</a>
+      </div>
+      <div className="description">
+        <p>
+          Free delivery on order over <span className="price">$200</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TopHeader;
--- src/Pages/Layout/index.jsx
+++ src/Pages/Layout/index.jsx
@@ -1,24 +1,22 @@
-import "../../Content/Style/custom.scss";
+import { Container } from "react-bootstrap";
+import TopHeader from "./component/topHeader";
+import Menu from "./component/menu";
+
+import "../../Content/Style/layout.scss";
+
 const Layout = ({ children }) => {
   return (
     <>
-      <div className="Header">
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">Contact Us</a>
-          </li>
-          <li>
-            <a href="#">Category</a>
-          </li>
-        </ul>
-      </div>
-      <div className="content">{children}</div>
-      <div className="Footer">
-        <h1>footer</h1>
-      </div>
+      <section className="topHeader">
+        <Container>
+          <TopHeader />
+        </Container>
+      </section>
+      <section className="menuHeader">
+        <Container>
+          <Menu />
+        </Container>
+      </section>
     </>
   );
 };

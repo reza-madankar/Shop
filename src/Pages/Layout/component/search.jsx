63296384b2e17d1152874d79d3002952dd024703
Mohammad Reza Madankar <88802498+rezapct@users.noreply.github.com>
2022-08-27 01:50:33 -0700

    Update Overly action

--- src/Pages/Layout/component/search.jsx
+++ src/Pages/Layout/component/search.jsx
@@ -1,16 +1,16 @@
-
 const SearchBox = (props) => {
-const makemistakes = () => {SearchBox(false)}
-    return (
-        
-        <>
-<div onClick={alert("Hello World!")} className="overlay" ></div>
-        <div className="searchBox">
-  <input type="text" placeholder="Search"></input>
-  <button type="submit">Submit</button>
-        </div>
-        </>
-    )
-  };
-  
-  export default SearchBox;
+  return (
+    <>
+      <div
+        onClick={() => props.setSearchVisible(false)}
+        className="overlay"
+      ></div>
+      <div className="searchBox">
+        <input type="text" placeholder="Search"></input>
+        <button type="submit">Submit</button>
+      </div>
+    </>
+  );
+};
+
+export default SearchBox;

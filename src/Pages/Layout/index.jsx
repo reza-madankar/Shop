13afbe3bbc671111a37f2d925a96e39df140c286
Mohammad Reza Madankar <88802498+rezapct@users.noreply.github.com>
2022-08-31 07:03:34 -0700

    Merge to main

--- src/Pages/Layout/index.jsx
+++ src/Pages/Layout/index.jsx
@@ -7,29 +7,16 @@ import "../../Content/Style/layout.scss";
 const Layout = ({ children }) => {
   return (
     <>
-      <div className="Header">
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">Home2</a>
-          </li>
-          <li>
-            <a href="#">Home3</a>
-          </li>
-          <li>
-            <a href="#">Contact Us</a>
-          </li>
-          <li>
-            <a href="#">Category</a>
-          </li>
-        </ul>
-      </div>
-      <div className="content">{children}</div>
-      <div className="Footer">
-        <h1>footer</h1>
-      </div>
+      <section className="topHeader">
+        <Container>
+          <TopHeader />
+        </Container>
+      </section>
+      <section className="menuHeader">
+        <Container>
+          <Menu />
+        </Container>
+      </section>
     </>
   );
 };

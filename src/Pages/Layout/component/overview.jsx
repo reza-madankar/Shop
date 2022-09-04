2dab67cb5e28f390f12d36392dee7ae3cdb6620c
Hooman <patriot_sh@yahoo.com>
2022-09-04 12:25:00 +0430

    ***Tasks*** 1-Just publish the overview.

---
+++ src/Pages/Layout/component/overview.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../../../Content/Style/overviewStyle.scss";
+function Overview() {
+    return ( 
+        <>
+        <div className="container">
+        
+            <div className="mainContent">
+      
+                <div className="content">
+                HOME ABOUT US
+                </div>
+            </div>
+        </div>
+        </>
+     );
+}
+
+export default Overview;
